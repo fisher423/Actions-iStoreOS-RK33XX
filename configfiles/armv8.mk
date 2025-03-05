afb316f745fbee9f5606c92bd59e46f8189f5403
ll1233 <68339285+fisher423@users.noreply.github.com>
2025-03-05 16:38:40 +0800

    Update armv8.mk

--- configfiles/armv8.mk
+++ configfiles/armv8.mk
@@ -93,3 +93,13 @@ define Device/rk3399_tpm312
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += rk3399_tpm312
+
+define Device/rumu3f_fine-3399
+  DEVICE_VENDOR := RUMU3F
+  DEVICE_MODEL := Fine 3399
+  SOC := rk3399
+  UBOOT_DEVICE_NAME := fine3399-rk3399
+  BOOT_FLOW := pine64-bin
+  DEVICE_PACKAGES := kmod-r8169 wpad-openssl brcmfmac-firmware-43430b0-sdio brcmfmac-nvram-43430b0-sdio
+endef
+TARGET_DEVICES += rumu3f_fine-3399
