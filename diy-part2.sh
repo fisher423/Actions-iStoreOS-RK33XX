e83b10ffb254369f16c14a92f9b1b2217b0d090b
ll1233 <68339285+fisher423@users.noreply.github.com>
2025-03-21 17:38:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -56,6 +56,15 @@ echo -e "\\ndefine Device/rk3399_tpm312
 endef
 TARGET_DEVICES += rk3399_tpm312" >> target/linux/rockchip/image/armv8.mk
 
+echo -e "\\ndefine Device/rumu3f_fine-3399
+  DEVICE_VENDOR := RUMU3F
+  DEVICE_MODEL := FINE 3399
+  SOC := rk3399
+  UBOOT_DEVICE_NAME := fine3399-rk3399
+  BOOT_FLOW := pine64-bin
+  DEVICE_PACKAGES := kmod-gpio-button-hotplug kmod-r8168
+endef
+TARGET_DEVICES += rumu3f_fine-3399" >> target/linux/rockchip/image/armv8.mk
 
 
 
