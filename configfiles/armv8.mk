c8df3600c41c98ba19c57302cab7689ae03dce53
ll1233 <68339285+fisher423@users.noreply.github.com>
2025-03-22 14:31:13 +0800

    Update armv8.mk

--- configfiles/armv8.mk
+++ configfiles/armv8.mk
@@ -3,16 +3,6 @@
 # Copyright (C) 2020 Sarah Maedel
 
 
-define Device/rockchip_rk3308_evb
-  DEVICE_VENDOR := Rochckip
-  DEVICE_MODEL := RK3308 EVB
-  SOC := rk3308
-  UBOOT_DEVICE_NAME := evb-rk3308
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script rk3308 | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152 ethtool
-endef
-TARGET_DEVICES += rockchip_rk3308_evb
-
 define Device/rumu3f_fine-3399
   DEVICE_VENDOR := RUMU3F
   DEVICE_MODEL := FINE-3399
