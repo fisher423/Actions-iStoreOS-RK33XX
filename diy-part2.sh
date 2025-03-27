50df7f9d20a914e6b78f3b1953836e74cdcec5bf
ll1233 <68339285+fisher423@users.noreply.github.com>
2025-03-27 15:04:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -55,15 +55,17 @@ echo -e "\\ndefine Device/rk3399_tpm312
 endef
 TARGET_DEVICES += rk3399_tpm312" >> target/linux/rockchip/image/armv8.mk
 
-echo -e "\\ndefine Device/rk3399_fine3399
-  DEVICE_VENDOR := RK3399
-  DEVICE_MODEL := FINE3399
+echo -e "\\ndefine Device/rumu3f_fine-3399
+  DEVICE_VENDOR := RUMU3F
+  DEVICE_MODEL := Fine 3399
   SOC := rk3399
-  SUPPORTED_DEVICES := rk3399,fine3399
-  UBOOT_DEVICE_NAME := fine3399-rk3399
+  SUPPORTED_DEVICES := rk3399,fine-3399
+  UBOOT_DEVICE_NAME := fine-3399-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  BOOT_FLOW := pine64-bin
+  DEVICE_PACKAGES := kmod-r8169
 endef
-TARGET_DEVICES += rk3399_fine3399" >> target/linux/rockchip/image/armv8.mk
+TARGET_DEVICES += rumu3f_fine-3399" >> target/linux/rockchip/image/armv8.mk
 
 ls target/linux/rockchip/image
 cat target/linux/rockchip/image/armv8.mk
